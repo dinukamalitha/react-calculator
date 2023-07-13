c67e200ace6e0aa12183880ce33f2e48b6879894
Dinuka Malitha <dmcoder01@gmail.com>
2023-07-13 16:45:22 +0530

    Second Commit

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
+import * as Math from "mathjs";
 
-function Calculator(){
+function Calculator() {
 
     const [displayValue, setDisplayValue] = useState("");
     const [answer, setAnswer] = useState(0);
 
-    function insert(e){
+    function insert(e) {
         const value = e.target.textContent;
         setDisplayValue(prevValue => prevValue + value);
     }
 
-    function evaluate(){
-        const res = eval(displayValue);
-        setAnswer(res);
+    function evaluate() {
+        try {
+            const res = Math.evaluate(displayValue);
+            if (isNaN(res)) {
+                throw new Error("Syntax Error");
+            }
+            setAnswer(res);
+        } catch (error) {
+            setAnswer(error.message);
+        }
     }
 
-    function clear(){
+    function clear() {
+        setAnswer(0);
+    }
+
+    function clearAll() {
         setDisplayValue("");
         setAnswer(0);
     }
@@ -23,7 +35,7 @@ function Calculator(){
     return (
         <div>
             <h1>My Calculator</h1>
-            
+
             <div className="container">
                 <div className="display">
                     <div className="prev">{displayValue}</div>
@@ -31,22 +43,22 @@ function Calculator(){
                 </div>
                 <div className="keypad">
                     <div className="row">
-                        <div className="btn">.</div>
-                        <div className="btn" onClick={insert}>*</div>
-                        <div className="btn" onClick={insert}>/</div>
-                        <div className="btn"></div>
+                        <div className="btn" onClick={insert}>.</div>
+                        <div className="btn" onClick={insert}>(</div>
+                        <div className="btn" onClick={insert}>)</div>
+                        <div className="btn" onClick={clearAll}>CA</div>
                     </div>
                     <div className="row">
                         <div className="btn" onClick={insert}>7</div>
                         <div className="btn" onClick={insert}>8</div>
                         <div className="btn" onClick={insert}>9</div>
-                        <div className="btn" onClick={insert}>-</div>
+                        <div className="btn" onClick={insert}>*</div>
                     </div>
                     <div className="row">
                         <div className="btn" onClick={insert}>4</div>
                         <div className="btn" onClick={insert}>5</div>
                         <div className="btn" onClick={insert}>6</div>
-                        <div className="btn" onClick={insert}>+</div>
+                        <div className="btn" onClick={insert}>/</div>
                     </div>
                     <div className="row">
                         <div className="btn" onClick={insert}>1</div>
@@ -55,9 +67,9 @@ function Calculator(){
                         <div className="btn" onClick={clear}>C</div>
                     </div>
                     <div className="row">
-                        <div className="btn"></div>
+                        <div className="btn" onClick={insert}>-</div>
                         <div className="btn" onClick={insert}>0</div>
-                        <div className="btn"></div>
+                        <div className="btn" onClick={insert}>+</div>
                         <div className="btn" onClick={evaluate}>=</div>
                     </div>
                 </div>
